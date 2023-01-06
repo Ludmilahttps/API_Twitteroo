a474beed9c4151b5f67dfe796ed39af8a545e277
LudmilaS <ludmila.https@gmail.com>
2023-01-06 14:41:04 -0300

    falta implementar com o front

--- src/app.js
+++ src/app.js
@@ -1,28 +1,50 @@
 import express from 'express'
+import cors from "cors"
 
 const server = express()
 server.use(express.json())
+server.use(cors())
 const PORT = 5000
 
 let users = []
+let tweets = []
 
 server.listen(PORT, () => {
-  console.log('Helooo')
+  console.log('Im a server')
 })
 
 server.get("/tweets", (request, response) => {
   console.log("get tweets")
-  response.send(users)
+  tweets.reverse()
+
+  if (tweets.length > 10) {
+    tweets.length = 10
+  }
+
+  res.send(tweets)
+  tweets.reverse()
 })
 
 server.post('/tweets', (req, res) => {
-  const pessoa = req.body
-  users.push(pessoa)
-  res.send(pessoa)
+  const tweet =
+  {
+    username: "",
+    avatar: "",
+    tweet: ""
+  }
+
+  tweet.username = req.body.username
+  tweet.avatar = users[users.length - 1].avatar
+  tweet.tweet = req.body.tweet
+
+  tweets.push(tweet);
+  res.send("Post");
 })
 
 server.post('/sign-up', (req, res) => {
-  const pessoa = req.body
-  users.push(pessoa)
-  res.send(pessoa)
+  const people = req.body
+  users.push(people)
+  res.send(people)
+
+  //res.send("Save info")
 })
