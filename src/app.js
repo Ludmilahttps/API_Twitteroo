465f206232776c7aa2a3333be7a48d9320fcc53a
LudmilaS <ludmila.https@gmail.com>
2023-01-07 22:51:34 -0300

    funciona pfvr

--- src/app.js
+++ src/app.js
@@ -29,30 +29,18 @@ server.get("/tweets", (request, response) => {
 //salva os tweets
 server.post('/tweets', (request, response) => {
   console.log("post tweets")
-
-  const newTweet =
-  {
-    username: "",
-    avatar: "",
-    tweet: ""
-  }
-
-  newTweet.username = request.body.username
-  newTweet.avatar = users[users.length - 1].avatar
-  newTweet.tweet = request.body.tweet 
-
-  if (!newTweet.username || !newTweet.tweet|| typeof newTweet.tweet !== 'string') {
-    return response.status(401).send('Unauthorized')
+  if (!request.body.username || !request.body.tweet || typeof request.body.tweet !== 'string') {
+    return response.status(401).send('Todos os campos são obrigatórios!');
   }
 
   const user = request.body.username;
   const isSignedUp = users.some(({ username }) => username === user);
 
   if (!isSignedUp) {
-    return response.status(401).send('UNAUTHORIZED')
+    return response.status(401).send('UNAUTHORIZED');
   }
 
-  tweets.push(request.body)
+  tweets.push(request.body);
 
   response.status(201).send('OK');
 
